3bb395ae2086a5e13fceca7f19cb986c712b7ae2
Vladimir Zaguliaev <oinnwork@outlook.com>
2022-05-15 09:54:58 +0300

    Fixed UiInput

--- src/components/Core/UiInput.vue
+++ src/components/Core/UiInput.vue
@@ -1,13 +1,29 @@
 <template>
-  <input class="ui-input" type="text" :aria-label="label" :placeholder="placeholder"/>
+  <input
+    v-model="value"
+    class="ui-input"
+    type="text"
+    :aria-label="label"
+    :placeholder="placeholder"
+  />
 </template>
 
 <!--suppress JSUnusedGlobalSymbols -->
 <script lang="ts" setup> // UiInput
-import { defineProps } from 'vue';
+import { computed, defineEmits, defineProps } from 'vue';
+
+interface Emits {
+  (e: 'update:modelValue', value: string): void,
+}
+
+const emit = defineEmits<Emits>();
 
 // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
 const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
   label: {
     type: String,
     required: true,
@@ -15,6 +31,15 @@ const props = defineProps({
   placeholder: {
     type: String,
     default: '',
+  },
+});
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(text: string) {
+    emit('update:modelValue', text);
   },
 });
 </script>
